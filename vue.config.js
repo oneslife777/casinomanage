0b6f708e778fbabfc9b357a5154698a785a43a57
oneslife777 <crown1515@gmail.com>
2021-03-15 08:30:36 +0900

    production用の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  publicPath: isProd ? '/casinotools' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
